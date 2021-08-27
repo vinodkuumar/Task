f64c8d61aca5e08f9ac73568ad2ecd65fe397b7b
Vinodkumar <vinodkumarmuppera@gmail.com>
2021-08-27 15:53:41 +0530

    without usecontext

--- App.js
+++ App.js
@@ -93,35 +93,76 @@
 //   },
 // });
 
-import React, {useState, useContext, createContext,useEffect} from 'react';
-import {View, Text, StyleSheet, Button, BackHandler} from 'react-native';
+// import React, {useState, useContext, createContext,useEffect} from 'react';
+// import {View, Text, StyleSheet, Button, BackHandler} from 'react-native';
 
-const Context = createContext('default value');
+// const Context = createContext('default value');
 
-export default function App() {
+// export default function App() {
+//   const [count, setCount] = useState(0);
+//   useEffect(() => {
+//     const backAction = () => {
+//       setCount(count + 1);
+//       alert(`you clicked ${count} times`);
+//       return true;
+//     }
+//     const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction)
+//     return () => backHandler.remove();
+//   },[count])
+//   return (
+//   <Context.Provider value={count}>
+//     <ChildComponent />
+//   </Context.Provider>);
+// }
+
+// function ChildComponent(props) {
+//   var contextData = useContext(Context);
+//   return (
+//     <View>
+//       <Context.Consumer>
+//         {data => <Text>count here is : {data} </Text>}
+//       </Context.Consumer>
+//     </View>
+//   );
+// }
+
+
+import React, {useState,useEffect} from 'react';
+import {Text, View, StyleSheet, BackHandler, Alert} from 'react-native';
+
+const App = () => {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    const backAction = () => {
-      setCount(count + 1);
-      alert(`you clicked ${count} times`);
-      return true;
-    }
-    const backHandler = BackHandler.addEventListener("hardwareBackPress", backAction)
-    return () => backHandler.remove();
-  },[count])
-  return (
-  <Context.Provider value={count}>
-    <ChildComponent />
-  </Context.Provider>);
-}
+  const increaseCount = () => {
+    setCount(count + 1);
+    alert(`you clicked ${count} times`);
+    return true;
+  }
 
-function ChildComponent(props) {
-  var contextData = useContext(Context);
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', increaseCount);
+
+    return () =>
+      BackHandler.removeEventListener('hardwareBackPress', increaseCount);
+  }, [count]);
+
   return (
-    <View>
-      <Context.Consumer>
-        {data => <Text>count here is : {data} </Text>}
-      </Context.Consumer>
+    <View style={styles.container}>
+      <Text style={styles.text}>{count}</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white'
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
